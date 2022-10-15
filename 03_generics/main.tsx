18b179379009a8b1196dafc634dcbc692e89b4fb
Hlushak Dzmitry <hlushak.dzmitry@gmail.com>
2022-10-15 18:31:07 +0300

    change: update 'generics'

--- 03_generics/main.tsx
+++ 03_generics/main.tsx
@@ -211,3 +211,25 @@ const component: FunctionComponent<{ name: string; age: number }> = ({
   name,
   age,
 }) => {};
+
+/**********************************************************************/
+// Получить значение из вложенного объекта (двухуровнего)
+const menu = {
+  analytics: {
+    bussines: "Для бизнеса",
+    data: "Big Data",
+  },
+  design: {
+    graphical: "Graphic",
+  },
+};
+
+const getMenu = <T extends object, L1 extends keyof T, L2 extends keyof T[L1]>(
+  obj: T,
+  l1: L1,
+  l2: L2
+) => {
+  return obj[l1][l2];
+};
+
+console.log(getMenu(menu, "analytics", "bussines"));
